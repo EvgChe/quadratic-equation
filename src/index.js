3b58797bd554d4ee825376440a1f69f5c26dfcf3
EvgChe <chetkova@list.ru>
2018-02-23 14:42:00 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+var temp = equation.split(' ').join('').split('*x');
+var a = parseInt(temp[0]);
+var b = parseInt(temp[1].replace('^2', ''));
+var c;
+var x = [];
+if(temp.length == 3){
+    c = parseInt(temp[2]);
+}
+else{
+    c = 0;
+}
+
+  var x_1, x_2;
+
+  var Discr =Math.pow(b,2)-(4*a*c);
+
+  if (Discr < 0) {
+    return NaN;
+  }
+  else if (Discr == 0) {
+    x_1 = x_2 = Math.round(-b/(2*a));
+
+  }
+  else {
+    x_1 = Math.round ((-b + Math.sqrt(Discr))/(2*a));
+    x_2 = Math.round ((-b - Math.sqrt(Discr))/(2*a));
+
+  }
+
+  x_1 > x_2 ? x.push(x_2) : x.push(x_1);
+  x_1 > x_2 ? x.push(x_1) : x.push(x_2);
+
+  return x;
 }
